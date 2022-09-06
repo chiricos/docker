741e0dfe661c89d134c75fdff239e871aef16508
edward diaz <edward.diaz@bitsamericas.com>
2022-09-06 11:01:03 -0500

    subiendo docker doc

---
+++ docker-commit/dump.sql
@@ -0,0 +1,39 @@
+-- MySQL dump 10.13  Distrib 8.0.30, for Linux (x86_64)
+--
+-- Host: 127.0.0.1    Database: sys
+-- ------------------------------------------------------
+-- Server version	5.7.39
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Temporary view structure for view `host_summary`
+--
+
+DROP TABLE IF EXISTS `host_summary`;
+/*!50001 DROP VIEW IF EXISTS `host_summary`*/;
+SET @saved_cs_client     = @@character_set_client;
+/*!50503 SET character_set_client = utf8mb4 */;
+/*!50001 CREATE VIEW `host_summary` AS SELECT 
+ 1 AS `host`,
+ 1 AS `statements`,
+ 1 AS `statement_latency`,
+ 1 AS `statement_avg_latency`,
+ 1 AS `table_scans`,
+ 1 AS `file_ios`,
+ 1 AS `file_io_latency`,
+ 1 AS `current_connections`,
+ 1 AS `total_connections`,
+ 1 AS `unique_users`,
+ 1 AS `current_memory`,
+ 1 AS `total_memory_allocated`*/;
+SET character_set_client = @saved_cs_client;
